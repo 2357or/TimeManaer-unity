d65e6653001f88142e8a09e3e70f20680e5117e9
大槻亮太 <ootsukiryouta@MacBook-Pro-13-inch-2018.local>
2020-03-11 09:21:24 +0900

    “ファイルを追加”

---
+++ CountDownTimer/CountDownTimer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownTimer : MonoBehaviour
+{
+    private float remainTime;    //トータル制限時間
+    [SerializeField] private int minute;    //　制限時間（分）
+    [SerializeField] private float seconds;   //　制限時間（秒）
+    [SerializeField] private string endMessage;
+
+    bool Count;     //タイマーを動かすかどうか
+
+    private Text timerText;     //時間を表示するテキスト
+
+    void Start()
+    {
+        //トータル制限時間をfloatの秒数に変換する
+        remainTime = minute * 60 + seconds;
+        //
+        //oldSeconds = 0f;
+        //Textコンポーネントを取得
+        timerText = GetComponent<Text>();
+
+        //タイマーを開始させるためtrueに
+        Count = true;
+        if (endMessage == null) { endMessage = "制限時間終了"; }
+    }
+
+    void Update()
+    {
+        //制限時間が0秒以下なら何もしない
+        if (remainTime <= 0f)
+        {
+            return;
+        }
+
+        //残りのトータル制限時間を計測；
+        remainTime -= Time.deltaTime;
+
+        minute = (int)remainTime / 60;
+        seconds = remainTime - minute * 60;
+
+        //タイマー表示用UIテキストの時間の表示を更新
+        if (Count) { timerText.text = minute.ToString("00") + ":" + ((int)seconds).ToString("00"); }
+
+        //タイマー終了
+        //制限時間以下になったらコンソールに『制限時間終了』と表示する
+        if (remainTime <= 0f)
+        {
+            ///
+            //タイマー終了時の処理をここに書く
+            ///
+            Debug.Log("制限時間終了");
+            timerText.text = endMessage;
+        }
+
+        //使わないなら削除すること↓
+        //マウスの左ボタン押しで一時停止
+        if (Input.GetMouseButton(0))
+        {
+            Count = !Count;
+        }
+    }
+}
---
+++ CountUpTimer/CountUpTimer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountUpTimer : MonoBehaviour
+{
+    private int minute;
+    private float seconds;
+
+    Text timerText;     //タイマー表示用テキスト
+    bool Count;     //タイマーを動かすかどうか
+
+    void Start()
+    {
+        // 初期化
+        minute = 0;
+        seconds = 0f;
+
+        //Textコンポーネントを取得
+        timerText = GetComponent<Text>();
+
+        //タイマーを開始させるためtrueに
+        Count = true;
+    }
+
+    void Update()
+    {
+        //時間を加算
+        if (Count) { seconds += Time.deltaTime; }
+
+        //秒数が60秒を超えた時、1分に変換する
+        if (seconds >= 60f)
+        {
+            minute++;
+            seconds = seconds - 60;
+        }
+
+        //タイマー表示用UIテキストの時間の表示を更新
+        if (Count) { timerText.text = minute.ToString("00") + ":" + ((int)seconds).ToString("00"); }
+        
+
+        //使わないなら削除すること↓
+        //マウスの左ボタン押しで一時停止
+        if (Input.GetMouseButton(0))
+        {
+            Count = !Count;
+        }
+    }
+}
